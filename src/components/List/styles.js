4154b60aed683b689833644efb6d5815fd4df5da
Leonardo Viana <leonardo.viana@meep-app.com>
2019-10-21 09:54:45 -0300

    Finishing our App

--- src/components/List/styles.js
+++ src/components/List/styles.js
@@ -4,6 +4,7 @@ export const Container = styled.div`
     padding: 0 15px;
     height: 100%;
     flex: 0 0 320px;
+    opacity:${props=>props.done?0.6:1};
 
     & + div{
         border-left: 1px solid rgba(0, 0, 0, 0.05)
@@ -23,7 +24,7 @@ export const Container = styled.div`
       button{
           width:42px;
           height:42px;
-          border-radius:18px
+          border-radius:18px;
           background: #D93B48;
           border:0;
           cursor: pointer;
